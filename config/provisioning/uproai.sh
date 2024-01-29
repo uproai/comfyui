c1528ae3c7747af909404cbd04f7522ac9b0d754
Yuan He <yuan@linux.com>
2024-01-29 17:01:08 +0800

    add uproai.sh comfyui init.sh

---
+++ config/provisioning/uproai.sh
@@ -0,0 +1,243 @@
+#!/bin/false
+
+# This file will be sourced in init.sh
+
+# https://raw.githubusercontent.com/ai-dock/comfyui/main/config/provisioning/default.sh
+
+
+NODES=(
+    "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet"
+    "https://github.com/Kosinkadink/ComfyUI-AnimateDiff-Evolved"
+    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    "https://github.com/pythongosssss/ComfyUI-WD14-Tagger"
+    "https://github.com/EllangoK/ComfyUI-post-processing-nodes"
+    "https://github.com/RockOfFire/ComfyUI_Comfyroll_CustomNodes"
+    "https://github.com/FizzleDorf/ComfyUI_FizzNodes"
+    "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+    "https://github.com/lilly1987/ComfyUI_node_Lilly"
+    "https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes"
+    "https://github.com/laksjdjf/IPAdapter-ComfyUI"
+    "https://github.com/AIrjen/OneButtonPrompt"
+    "https://github.com/civitai/comfy-nodes"
+    "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    "https://github.com/storyicon/comfyui_segment_anything"
+    "https://github.com/Stability-AI/stability-ComfyUI-nodes"
+    "https://github.com/WASasquatch/was-node-suite-comfyui"
+    "https://github.com/Fannovel16/ComfyUI-Frame-Interpolation"
+    "https://github.com/wallish77/wlsh_nodes"
+    "https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite"
+    "https://github.com/M1kep/Comfy_KepMatteAnything"
+    "https://github.com/Ttl/ComfyUi_NNLatentUpscale"
+    "https://github.com/Niutonian/ComfyUi-NoodleWebcam"
+    "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
+    "https://github.com/WASasquatch/PowerNoiseSuite"
+    "https://github.com/rgthree/rgthree-comfy"
+    "https://github.com/AbyssYuan0/ComfyUI_BadgerTools"
+    "https://github.com/Nourepide/ComfyUI-Allor"
+    "https://github.com/Gourieff/comfyui-reactor-node"
+    "https://github.com/sipherxyz/comfyui-art-venture"
+    "https://github.com/daxthin/facedetailer"
+    "https://github.com/BadCafeCode/masquerade-nodes-comfyui"
+    "https://github.com/BlenderNeko/ComfyUI_Noise"
+    "https://github.com/BlenderNeko/ComfyUI_ADV_CLIP_emb"
+    "https://github.com/flyingshutter/As_ComfyUI_CustomNodes"
+    "https://github.com/TinyTerra/ComfyUI_tinyterraNodes"
+    "https://github.com/kohya-ss/ControlNet-LLLite-ComfyUI"
+    "https://github.com/mihaiiancu/ComfyUI_Inpaint"
+    "https://github.com/kwaroran/abg-comfyui"
+    "https://github.com/LEv145/images-grid-comfy-plugin"
+    "https://github.com/cubiq/ComfyUI_essentials"
+    "https://github.com/thecooltechguy/ComfyUI-MagicAnimate"
+    "https://github.com/MrForExample/ComfyUI-AnimateAnyone-Evolved"
+    "https://github.com/chaojie/ComfyUI-Moore-AnimateAnyone"
+    "https://github.com/kijai/ComfyUI-KJNodes"
+    "https://github.com/melMass/comfy_mtb"
+    "https://github.com/shadowcz007/comfyui-mixlab-nodes"
+    "https://github.com/Jcd1230/rembg-comfyui-node"
+    "https://github.com/Shraknard/ComfyUI-Remover"
+)
+
+
+
+CHECKPOINT_MODELS=(
+    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    # civitai checkpoints
+    "https://civitai.com/api/download/models/90854"
+    "https://civitai.com/api/download/models/94654"
+    "https://civitai.com/api/download/models/266360"
+    "https://civitai.com/api/download/models/32425"
+    "https://civitai.com/api/download/models/128713"
+    "https://civitai.com/api/download/models/138124"
+    "https://civitai.com/api/download/models/119057"
+    "https://civitai.com/api/download/models/79337"
+    "https://civitai.com/api/download/models/265102"
+    "https://civitai.com/api/download/models/265102"
+    "https://civitai.com/api/download/models/119057"
+    "https://civitai.com/api/download/models/266360"
+    "https://civitai.com/api/download/models/32425"
+
+    "https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
+    "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
+    "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
+)
+
+LORA_MODELS=(
+    #"https://civitai.com/api/download/models/16576"
+    "https://civitai.com/api/download/models/230410"
+    "https://civitai.com/api/download/models/33016"
+    "https://civitai.com/api/download/models/230410"
+    "https://civitai.com/api/download/models/265076"
+    "https://civitai.com/api/download/models/13050"
+    "https://civitai.com/api/download/models/99996"
+    "https://civitai.com/api/download/models/25156"
+    "https://civitai.com/api/download/models/113529"
+    "https://civitai.com/api/download/models/45113"
+    "https://civitai.com/api/download/models/109019"
+    "https://civitai.com/api/download/models/15380"
+    "https://civitai.com/api/download/models/88113"
+    "https://civitai.com/api/download/models/38487"
+    "https://civitai.com/api/download/models/78614"
+    "https://civitai.com/api/download/models/151467"
+    "https://civitai.com/api/download/models/171075"
+    "https://civitai.com/api/download/models/123926"
+    "https://civitai.com/api/download/models/132846"
+    "https://civitai.com/api/download/models/11231"
+    "https://civitai.com/api/download/models/118229"
+    "https://civitai.com/api/download/models/230410"
+    "https://civitai.com/api/download/models/121571"
+    "https://civitai.com/api/download/models/5544"
+    "https://civitai.com/api/download/models/92856"
+    "https://civitai.com/api/download/models/33016"
+    "https://civitai.com/api/download/models/66114"
+    "https://civitai.com/api/download/models/9860"
+    "https://civitai.com/api/download/models/86759"
+    "https://civitai.com/api/download/models/57026"
+    "https://civitai.com/api/download/models/65080"
+    "https://civitai.com/api/download/models/45082"
+    "https://civitai.com/api/download/models/218571"
+    "https://civitai.com/api/download/models/42665"
+    "https://civitai.com/api/download/models/26704"
+    "https://civitai.com/api/download/models/261625"
+    "https://civitai.com/api/download/models/180296"
+)
+
+VAE_MODELS=(
+    "https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
+)
+
+ESRGAN_MODELS=(
+    "https://huggingface.co/ai-forever/Real-ESRGAN/resolve/main/RealESRGAN_x4.pth"
+    "https://huggingface.co/FacehugmanIII/4x_foolhardy_Remacri/resolve/main/4x_foolhardy_Remacri.pth"
+    "https://huggingface.co/Akumetsu971/SD_Anime_Futuristic_Armor/resolve/main/4x_NMKD-Siax_200k.pth"
+)
+
+CONTROLNET_MODELS=(
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors"
+    "https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_depth_fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_hed-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_mlsd-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_normal-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_openpose-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_scribble-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_seg-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_canny-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_color-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_depth-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_keypose-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_openpose-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_seg-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_sketch-fp16.safetensors"
+    "https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_style-fp16.safetensors"
+)
+
+### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
+
+function provisioning_start() {
+    DISK_GB_AVAILABLE=$(($(df --output=avail -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_USED=$(($(df --output=used -m "${WORKSPACE}" | tail -n1) / 1000))
+    DISK_GB_ALLOCATED=$(($DISK_GB_AVAILABLE + $DISK_GB_USED))
+    provisioning_print_header
+    provisioning_get_nodes
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/ckpt" \
+        "${CHECKPOINT_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/lora" \
+        "${LORA_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/controlnet" \
+        "${CONTROLNET_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/vae" \
+        "${VAE_MODELS[@]}"
+    provisioning_get_models \
+        "${WORKSPACE}/storage/stable_diffusion/models/esrgan" \
+        "${ESRGAN_MODELS[@]}"
+    provisioning_print_end
+}
+
+function provisioning_get_nodes() {
+    for repo in "${NODES[@]}"; do
+        dir="${repo##*/}"
+        path="/opt/ComfyUI/custom_nodes/${dir}"
+        requirements="${path}/requirements.txt"
+        if [[ -d $path ]]; then
+            if [[ ${AUTO_UPDATE,,} != "false" ]]; then
+                printf "Updating node: %s...\n" "${repo}"
+                ( cd "$path" && git pull )
+                if [[ -e $requirements ]]; then
+                    micromamba -n comfyui run ${PIP_INSTALL} -r "$requirements"
+                fi
+            fi
+        else
+            printf "Downloading node: %s...\n" "${repo}"
+            git clone "${repo}" "${path}" --recursive
+            if [[ -e $requirements ]]; then
+                micromamba -n comfyui run ${PIP_INSTALL} -r "${requirements}"
+            fi
+        fi
+    done
+}
+
+function provisioning_get_models() {
+    if [[ -z $2 ]]; then return 1; fi
+    dir="$1"
+    mkdir -p "$dir"
+    shift
+    if [[ $DISK_GB_ALLOCATED -ge $DISK_GB_REQUIRED ]]; then
+        arr=("$@")
+    else
+        printf "WARNING: Low disk space allocation - Only the first model will be downloaded!\n"
+        arr=("$1")
+    fi
+
+    printf "Downloading %s model(s) to %s...\n" "${#arr[@]}" "$dir"
+    for url in "${arr[@]}"; do
+        printf "Downloading: %s\n" "${url}"
+        provisioning_download "${url}" "${dir}"
+        printf "\n"
+    done
+}
+
+function provisioning_print_header() {
+    printf "\n##############################################\n#                                            #\n#          Provisioning container            #\n#                                            #\n#         This will take some time           #\n#                                            #\n# Your container will be ready on completion #\n#                                            #\n##############################################\n\n"
+    if [[ $DISK_GB_ALLOCATED -lt $DISK_GB_REQUIRED ]]; then
+        printf "WARNING: Your allocated disk size (%sGB) is below the recommended %sGB - Some models will not be downloaded\n" "$DISK_GB_ALLOCATED" "$DISK_GB_REQUIRED"
+    fi
+}
+
+function provisioning_print_end() {
+    printf "\nProvisioning complete:  Web UI will start now\n\n"
+}
+
+# Download from $1 URL to $2 file path
+function provisioning_download() {
+    wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1"
+}
+
+provisioning_start
